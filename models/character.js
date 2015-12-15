60088f264101fe9178398324b4a23a9e2bfb35c2
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-15 21:52:40 +0000

    Removed validations for seeding.

--- models/character.js
+++ models/character.js
@@ -1,9 +1,9 @@
 var mongoose  = require('mongoose');
 
 var CharacterSchema = mongoose.Schema({
-  String              : { type: String, required: true },
-  kMandarin           : { type: String, required: true },
-  kDefinition         : { type: String, required: true },
+  String              : { type: String },
+  kMandarin           : { type: String },
+  kDefinition         : { type: String },
   kFrequency          : String,
   kTraditionalVariant : String
 });
