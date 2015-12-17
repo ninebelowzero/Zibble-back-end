64310ebbcd9e7442108013f1db08ed428e37f3a2
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-17 12:48:34 +0000

    Improved error messages with wrong password etc.

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -20,12 +20,13 @@ function register(req, res, next){
 }
 
 function login (req, res, next){
+  console.log(req.body);
   User.findOne({
     username: req.body.username
   }, function(err, user){
     if (err) return res.status(500).send(err);
-    if (!user) return res.status(403).send({ message: "Username not found." });
-    if (!user.validPassword(req.body.password)) return res.status(403).send({ message: "Wrong password."});
+    if (!user) return res.status(401).send({ message: "Incorrect username or password." });
+    if (!user.validPassword(req.body.password)) return res.status(401).send({ message: "Incorrect username or password."});
 
     var token = jwt.sign(user, secret, { expiresInMinutes: 10080 });
 
